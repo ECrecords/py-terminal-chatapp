34f9be1b8a9ac1fe05948dead980b80e547c4a64
echinoislas <elvischino754@gmail.com>
2022-10-13 14:29:30 -0700

    error handling

--- terminal_chatapp.py
+++ terminal_chatapp.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
 from logging import exception
-from logging.config import listen
 import socket
 import selectors
 import sys
+from time import sleep
 import traceback
 from typing import Union
 from requests import get
@@ -15,40 +15,47 @@ id = 0
 class PROGRAM_EXIT(Exception):
     pass
 
+# wrappper used to hold the selection menu of the chat applciation
 def menu(selector: selectors.DefaultSelector, connection_list: list, listen: socket.socket):
 
+    # reads input from stdin and strips whitespaces
     input = (sys.stdin.readline()).rstrip()
 
-    input = input.split(" ")
-    if input[0] == "help":
-        help()
-    elif input[0] == "myip":
-        print(f"The IP address is {get_ip()}")
-    elif input[0] == "myport":
-        print(f"The program runs on port number {get_port(listen)}")
-    elif input[0] == "connect":
-        connect(selector, connection_list, input[1], int(input[2], listen))
-    elif input[0] == "list":
-        list_connections(selector, connection_list)
-    elif input[0] == "send":
-        send_message(selector, connection_list, int(input[1]), " ".join(input[2:len(input)]))
-    elif input[0] == "terminate":
-        terminate(selector, connection_list, int(input[1]))
-    elif input[0] == "exit":
-        exit_program(selector, connection_list)
-        raise PROGRAM_EXIT
-    else:
-        print(  "invalid command: use command "
-                "help to display valid commands" )
-    
-    return selector, connection_list
+    # catch error to prevent app crash
+    try:
+        # splits the string by " " so addtional input arguments can be read.
+        input = input.split(" ")
+        if input[0] == "help":
+            help()
+        elif input[0] == "myip":
+            print(f"The IP address is {get_ip()}")
+        elif input[0] == "myport":
+            print(f"The program runs on port number {get_port(listen)}")
+        elif input[0] == "connect":
+            connect(selector, connection_list, input[1], int(input[2]), listen)
+        elif input[0] == "list":
+            list_connections(selector, connection_list)
+        elif input[0] == "send":
+            send_message(selector, connection_list, int(input[1]), " ".join(input[2:len(input)]))
+        elif input[0] == "terminate":
+            terminate(selector, connection_list, int(input[1]))
+        elif input[0] == "exit":
+            exit_program(selector, connection_list)
+            raise PROGRAM_EXIT
+        else:
+            print(  "invalid command: use command "
+                    "help to display valid commands" )
+    except IndexError:
+        print(  "invalid usage: use command "
+                "help to display valid usage")
 
-
+# wrapper, display menu
 def help():
-    print("myip display IP address\n"
-          "connect connect to another peer \n"
-          "send send messages to peers\n"
-          "………….\nexit exit the program")
+    print("myip - display IP address\n"
+          "myport - display Port\n"
+          "connect <ip> <port> - connect to another peer \n"
+          "send <id> <msg> - send messages to peers\n"
+          "………….\nexit - exit the program")
 
 def get_ip() -> str:
     return socket.gethostbyname(socket.gethostname() + ".local")
@@ -70,12 +77,18 @@ def connect(selector: selectors.DefaultSelector, connection_list: list, ip: str,
         connection_list.append((id, sock))
         selector.register(sock, events, data=data)
 
+        sleep(0.1)
         sock.sendall(f"\n\r\n\rlisten {get_port(listen)}".encode())
         print(f"The connection to peer {ip} is succeessfully established;")
-        return selector, connection_list
+    except ConnectionError:
+        connection_list.remove((id, sock))
+        selector.unregister(sock)
+        print(f"The connection to peer failed;")
+
     except:
-        print(f"{exception}\nThe connection to peer was not established;")
-        return selector, connection_list
+        connection_list.remove((id, sock))
+        selector.unregister(sock)
+        traceback.print_exc()
 
 def list_connections(selector: selectors.DefaultSelector, connection_list: list):
     print(f"id:\tIP Addresss\tPort")
@@ -120,10 +133,13 @@ def terminate(selector: selectors.DefaultSelector, connection_list: list, conn_i
         target_sock.close()
         connection_list.remove((conn_id, target_sock))
 
-        return selector, connection_list
-    except Exception:
+    except BrokenPipeError:
+            target_sock.close()
+            selector.unregister(target_sock)
+            connection_list.remove((conn_id, target_sock))
+    except:
         traceback.print_exc()
-        return selector, connection_list
+
 
 def exit_program(selector: selectors.DefaultSelector, connection_list: list) -> Union[selectors.DefaultSelector, list]:
         for entry in connection_list:
@@ -166,7 +182,12 @@ def receive_msg(selector: selectors.DefaultSelector, connection_list: list, sock
             dec_rd = recv_data.decode()
             dec_rdata_sp = dec_rd.split(" ")
             if dec_rdata_sp[0] == "\n\r\n\rlisten":
-                (selector.get_key(sock)).data.port = int(dec_rdata_sp[1])
+                sel_key = selector.get_key(sock)
+                id = sel_key.data.id
+                ip = sel_key.data.addr
+                port = dec_rdata_sp[1]
+                selector.unregister(sock)
+                selector.register(sock, selectors.EVENT_READ, data=Connection(id, ip, port))
                 return
             
             print(f"Message received from {data.addr}:")
@@ -216,11 +237,15 @@ def main():
         sel.close()
         exit()
     except (KeyboardInterrupt):
-        sel, conn_list = exit_program(sel, conn_list)
+        exit_program(sel, conn_list)
         lsocket.close()
         sel.close()
         exit()
     except Exception:
+        exit_program(sel, conn_list)
+        lsocket.close()
+        sel.close()
         traceback.print_exc()
+        exit()
 
 main()
